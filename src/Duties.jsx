a2d5ba15262086290aedcc1e2d40a6b0460048ad
zen <zenon.deviagge@gmail.com>
2024-03-14 18:52:39 -0300

    Display duties with icon. React mandatory key attribute on the components needs to be addressed.

--- src/Duties.jsx
+++ src/Duties.jsx
@@ -1,7 +1,14 @@
+import { FaAngleDoubleRight } from 'react-icons/fa'
 const Duties = ({duties}) => {
   console.log(duties);  
-  return (
-    <div>Duties</div>
-  )
+  return <div>
+    {duties.map((duty, index) => {
+      return <div key={index} className='job-desc'>
+        <FaAngleDoubleRight className='job-icon'/>
+        <p>{duty}</p>
+      </div>
+
+    })}
+  </div>
 }
 export default Duties
